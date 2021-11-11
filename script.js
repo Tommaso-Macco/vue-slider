72dd823987f285de81ba74acd2f7f46c860d694f
Tommaso <tommaso.macco@gmail.com>
2021-11-11 13:57:02 +0100

    first push

---
+++ script.js
@@ -0,0 +1,58 @@
+var carousel = new Vue ({
+    el: "#app",
+    data: {
+        carousel: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+})
+
+
+
+// const items = [
+//     'img/01.jpg',
+//     'img/02.jpg',
+//     'img/03.jpg',
+//     'img/04.jpg',
+//     'img/05.jpg'
+// ];
+
+// const title = [
+//     'Svezia',
+//     'Svizzera',
+//     'Gran Bretagna',
+//     'Germania',
+//     'Paradise'
+// ]
+
+// const text = [
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     'Lorem ipsum',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+// ]
